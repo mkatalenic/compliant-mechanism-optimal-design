d9b05eb6aa5f3dd95ced4d06d96dc1f9ed36e589
Stefan Ivić <stefan.ivic@gmail.com>
2022-08-30 14:59:07 +0200

    clam case tweaking

--- kljesta_3D_print_ivic/optimization_ggs.py
+++ kljesta_3D_print_ivic/optimization_ggs.py
@@ -64,8 +64,8 @@ max_volume = mesh.calculate_mechanism_volume()
 for node in mesh.node_laso(
         [
             (1e-3, 1e-3),
-            (51e-3, 1e-3),
-            (51e-3, -1e-3),
+            (71e-3, 1e-3),
+            (71e-3, -1e-3),
             (1e-3, -1e-3)
         ],
         only_main_nodes=True):
@@ -151,11 +151,14 @@ def min_fun(beam_widths, unique_str=None, debug=False):
             print('!' * 20)
             # input('Press return to continue.')
 
-        ccx_manipulator.used_mesh.current_displacement = displacement
-        ccx_manipulator.used_mesh.current_stress = vm_stress
+        np.savez_compressed(f'ccx_files/{unique_str}/data.npz', displacement=displacement, vm_stress=vm_stress)
+        # ccx_manipulator.used_mesh.current_displacement = displacement
+        # ccx_manipulator.used_mesh.current_stress = vm_stress
 
         errors = u_calc - u_goal
 
+        y_err_diff = np.abs(errors[0, 1] - errors[1, 1]) / np.average(np.abs(u_goal[:, 1]))
+        
         m = np.abs(u_goal) > 0
         errors[m] = np.abs(errors[m] / u_goal[m])
 
@@ -163,7 +166,7 @@ def min_fun(beam_widths, unique_str=None, debug=False):
         # y_err = np.sum(errors[:, 1]**2) #/ 25e-3 #* 2
         x_err = np.average(errors[:, 0]) #/ 100e-3 #* 6
         y_err = np.average(errors[:, 1]) #/ 25e-3 #* 2
-
+        
         if debug:
             print(f'{u_goal=}')
             print(f'{u_calc=}')
@@ -171,15 +174,15 @@ def min_fun(beam_widths, unique_str=None, debug=False):
             # print(f'{displacement=}')
             # print(f'{ccx_manipulator.final_displacement_node_positions=}')
 
-        return volume, x_err, y_err, 0
+        return volume, x_err, y_err, y_err_diff, 0
 
     else:
-        return np.nan, np.nan, np.nan, 1
+        return np.nan, np.nan, np.nan, np.nan, 1
 
 def eval_penalty(beam_widths, debug=False):
 
     unique_str = generateRandomAlphaNumericString(20)
-    volume, x_err, y_err, valid = min_fun(beam_widths, unique_str, debug)
+    volume, x_err, y_err, y_err_std, valid = min_fun(beam_widths, unique_str, debug)
     fit = volume * 0.001 + x_err + x_err * 0.001 + y_err * 0.998
 
     print(beam_widths)
@@ -198,9 +201,9 @@ optimizer.iterations = 5000
 optimizer.maximum_evaluations = 200000
 
 optimizer.evaluation_function = min_fun
-optimizer.objectives = 3
-optimizer.objective_labels = ['vol', 'x_err', 'y_err']
-optimizer.objective_weights = [0.001, 0.001, 0.998]
+optimizer.objectives = 4
+optimizer.objective_labels = ['vol', 'x_err', 'y_err', 'y_err_std']
+optimizer.objective_weights = [0.001, 0.001, 0.99, 0.008]
 optimizer.constraints = 1
 optimizer.constraint_labels = ['valid_sim']
 
@@ -219,7 +222,7 @@ optimizer.monitoring = 'dashboard'
 
 valid = False
 while not valid:
-    x0 = optimizer.ub #* 0.5
+    x0 = optimizer.ub * 0.5
     # x0 = optimizer.lb + np.random.random(dims) * (optimizer.ub - optimizer.lb)
     # x0[129] = 0
     #x0 = np.random.random(dims) * 4e-3 + 1.8e-3
@@ -227,14 +230,13 @@ while not valid:
         shutil.rmtree('ccx_files/x0')
     r = min_fun(x0, 'x0', debug=True)
     print(r)
-    valid = not r[3]
+    valid = not r[4]
 
 # input('Press return to continue.')
 test_dir = './ccx_files'
 
 # postavke animacije
 drawer = cv.mesh_drawer()
-drawer.from_object(mesh)
 drawer.my_figure.suptitle('Optimizacija kljesta')
 
 
@@ -266,23 +268,27 @@ def post_iteration_processing(it, candidates, best):
         # (since its directory is already renamed)
         candidates = np.delete(candidates, 0)
 
+        drawer.from_object(mesh)
         drawer.my_ax.clear()
         drawer.my_info_ax.clear()
 
+        npz = np.load(f'{test_dir}/best_it{it}/data.npz')
+        
         kljesta_info = {
             'Iteracija': int(it),
             'h ' + '[m]': f'{mesh.beam_height:.5E}',
             r'$dt_{j}$ ' + '[m]': f'{ccx_manipulator.used_mesh.final_displacement_array[:, 1:][:, 1]}',  # Traženi pomaci
-            r'$dd_{j}$ ' + '[m]': f'{ccx_manipulator.used_mesh.current_displacement[:, 1][ccx_manipulator.final_displacement_node_positions]}',  # Dobiveni pomaci
+            r'$dd_{j}$ ' + '[m]': f'{npz["displacement"][:, 1][ccx_manipulator.final_displacement_node_positions]}',  # Dobiveni pomaci
             'O:': candidates
             #r'$Volumen$': f'{candidates[0].O[0]}',
             #r'$x error$': f'{candidates[0].O[1]}',
             #r'$y error$ ': f'{candidates[0].O[2]}'
         }
 
+
         drawer.make_drawing(kljesta_info,
-                            ccx_manipulator.used_mesh.current_displacement,
-                            ccx_manipulator.used_mesh.current_stress,
+                            npz['displacement'],
+                            npz['vm_stress'],
                             (51e6, 0),
                             displacement_scale=1,
                             beam_names=False)
