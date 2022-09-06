2e8eea6e4f6569e451934330dfbad10de6657587
Mihael Katalenić <50487294+mkatalenic@users.noreply.github.com>
2022-09-06 22:34:44 +0200

    Update GGS.sh

--- kljesta_isprobavanje_bura/GGS.sh
+++ kljesta_isprobavanje_bura/GGS.sh
@@ -4,7 +4,7 @@ no_divisions=10
 
 for ((i=1; i<=$no_divisions; i+=1))
 do
-    python optimization_ggs.py $no_divisions $i
+    python3.9 optimization_ggs.py $no_divisions $i
     ime='GGS_'
     ext='.tar.gz'
     tar -cpzf $ime$i$ext "ccx_files" "img" "mesh_setup.pkl"
