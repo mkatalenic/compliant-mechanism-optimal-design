02f0428650dc4015e5c19ed7cecd950bf0eb6473
Mihael Katalenić <50487294+mkatalenic@users.noreply.github.com>
2022-09-06 22:45:32 +0200

    Update pokreni_me.sh

--- kljesta_isprobavanje_bura/pokreni_me.sh
+++ kljesta_isprobavanje_bura/pokreni_me.sh
@@ -5,4 +5,12 @@
 #SBATCH --ntasks=48
 #SBATCH --mem-per-cpu=500M
 
-srun bash GGS.sh
+no_divisions=10
+
+for ((i=1; i<=$no_divisions; i+=1))
+do
+    python3.9 optimization_ggs.py $no_divisions $i
+    ime='GGS_'
+    ext='.tar.gz'
+    tar -cpzf $ime$i$ext "ccx_files" "img" "mesh_setup.pkl"
+done
