0c9a3617e7deac68c561040f62875cb7feb95ad4
Mihael Katalenić <mihael.katalenic@proton.me>
2022-10-11 23:55:21 +0200

    promjena I guess

--- calculix_manipulation.py
+++ calculix_manipulation.py
@@ -106,6 +106,7 @@ def read_ccx_results(
         for displacement_per_node, stress_per_node in zip(
             displacement_list, stress_list
         ):
+<<<<<<< HEAD
             displacement_array = np.append(
                 displacement_array,
                 np.reshape(np.array(displacement_per_node, dtype=np.float64), (1, 3)),
@@ -115,6 +116,219 @@ def read_ccx_results(
                 stress_array,
                 np.reshape(np.array(stress_per_node, dtype=np.float64), (1, 6)),
                 axis=0,
+=======
+            os.mkdir(os.path.join(os.getcwd(), 'ccx_files'))
+
+    final_displacement_node_positions = np.empty(shape=(0),
+                                                 dtype=int)
+
+    force_node_positions = np.empty(shape=(0),
+                                    dtype=int)
+
+    boundary_node_positions = np.empty(shape=(0),
+                                       dtype=int)
+
+    def translate_mesh(self):
+
+        output_string = ''
+
+        beams_to_write = self.used_mesh.beam_array[
+            [width > 0 for width in self.used_mesh.beam_width_array]
+        ]
+
+        nodes_to_write = np.empty((0), dtype=np.int32)
+        for idx, beam in enumerate(self.used_mesh.beam_array):
+            for beam_to_write in beams_to_write:
+                if beam[0] == beam_to_write[0] and beam[1] == beam_to_write[1]:
+                    nodes_to_write = np.append(
+                        nodes_to_write,
+                        self.used_mesh._fetch_beam_nodes(idx)
+                    )
+
+        nodes_to_write = np.unique(nodes_to_write)
+        nodes_idx_to_write = nodes_to_write + 1
+        self._no_of_used_nodes = np.size(nodes_to_write)
+
+        # Node translator
+        output_string += '*node, nset=nall\n'
+        node_counter = 0
+        for string in [
+            f'{i}, {np.array2string(row, separator=",")[1:-1]}\n'
+            for i, row in zip(nodes_idx_to_write,
+                              self.used_mesh.node_array[nodes_to_write]
+                              )
+        ]:
+            node_counter += 1
+            for node, _, _ in self.used_mesh.final_displacement_array:
+                node = int(node)
+                if node == int(string.split(',')[0]) and \
+                   node_counter not in self.final_displacement_node_positions and \
+                   np.shape(self.used_mesh.final_displacement_array)[0] != self.final_displacement_node_positions.size:
+                    self.final_displacement_node_positions = np.append(
+                        self.final_displacement_node_positions,
+                        node_counter
+                    )
+
+            for node in self.used_mesh.force_array[:, 0]:
+                if node + 1 == int(string.split(',')[0]) and \
+                   node_counter not in self.force_node_positions:
+                    self.force_node_positions = np.append(
+                        self.force_node_positions,
+                        node_counter
+                    )
+
+            for node in self.used_mesh.boundary_array[:, 0]:
+                if node + 1 == int(string.split(',')[0]) and \
+                   node_counter not in self.boundary_node_positions:
+                    self.boundary_node_positions = np.append(
+                        self.boundary_node_positions,
+                        node_counter
+                    )
+
+            output_string += string
+
+        # Beam translator
+        elset_name_list: list[str] = []
+        written_beam_index = 0
+        element_index = 0
+        for idx, beam in enumerate(self.used_mesh.beam_array):
+            for beam_to_write in beams_to_write:
+                if beam[0] == beam_to_write[0] and beam[1] == beam_to_write[1]:
+                    written_beam_index += 1
+                    elset_name = f'b_{written_beam_index}'
+                    elset_name_list.append(elset_name)
+
+                    output_string += f'*element,type=b32,elset={elset_name}\n'
+
+                    beam_elements = np.reshape(
+                        self.used_mesh._fetch_beam_nodes(idx)[:-1],
+                        (-1, 2)
+                    )
+                    beam_elements = np.append(
+                        beam_elements,
+                        np.reshape(
+                            np.append(
+                                beam_elements[1:, 0],
+                                self.used_mesh._fetch_beam_nodes(idx)[-1]
+                            ),
+                            (-1, 1)
+                        ),
+                        axis=1
+                    )
+                    beam_elements += 1
+
+                    for element in beam_elements:
+                        element_index += 1
+                        nodes_per_el_str = np.array2string(
+                            element,
+                            separator=','
+                        )[1:-1]
+                        output_string += f'{element_index},' +\
+                            f' {nodes_per_el_str}\n'
+
+        output_string += '*elset, elset=elall\n'
+        for name in elset_name_list:
+            output_string += f'{name},\n'
+
+        # Materials writer
+        output_string += '*material, name=mesh_material\n'
+        output_string += '*elastic, type=iso\n'
+        output_string += f'{self.used_mesh.material}'[1:-1] + '\n'
+
+        # Beam width writer
+        widths_to_write = self.used_mesh.beam_width_array[
+            [width > 0 for width in self.used_mesh.beam_width_array]
+        ]
+        for element_set_name, width in zip(elset_name_list, widths_to_write):
+            output_string += f'*beam section, elset={element_set_name},'
+            output_string += 'material=mesh_material, section=rect\n'
+            output_string += f'{self.used_mesh.beam_height}, {width:.20f}\n'
+            output_string += '0.d0,0.d0,1.d0\n'
+
+        # 2D case definition
+        output_string += '*boundary\n'
+        for node in nodes_idx_to_write:
+            output_string += f'{node}, 3,5\n'
+
+        # Boundary writer
+        output_string += '*boundary\n'
+        for boundary_definition in self.used_mesh.boundary_array:
+            idx, x_ax, y_ax, z_rot = boundary_definition
+            idx += 1
+            if x_ax != 0:
+                output_string += f'{idx}, {x_ax}\n'
+            if y_ax != 0:
+                output_string += f'{idx}, {y_ax*2}\n'
+            if z_rot != 0:
+                output_string += f'{idx}, {z_rot*6}\n'
+
+        # Initial displacement writer
+        if np.size(self.used_mesh.initial_displacement_array) != 0:
+            output_string += '*initial conditions,type=displacement\n'
+        for (idx, disp_x, disp_y) in self.used_mesh.initial_displacement_array:
+            disp_x_sting = f'{int(idx + 1)}, 1, {disp_x}\n'
+            disp_y_sting = f'{int(idx + 1)}, 2, {disp_y}\n'
+            if disp_x != 0:
+                output_string += disp_x_sting
+            if disp_y != 0:
+                output_string += disp_y_sting
+
+        # Linear/nonlinear solver
+        if self.nonlinear_calculation:
+            output_string += '*step, nlgeom\n*static\n'
+        else:
+            output_string += '*step\n*static\n'
+
+        # Force writer
+        if np.size(self.used_mesh.force_array) != 0:
+            output_string += '*cload\n'
+        for (idx, force_x, force_y) in self.used_mesh.force_array:
+            force_x_sting = f'{int(idx + 1)}, 1, {force_x}\n'
+            force_y_sting = f'{int(idx + 1)}, 2, {force_y}\n'
+            if force_x != 0:
+                output_string += force_x_sting
+            if force_y != 0:
+                output_string += force_y_sting
+
+        # File tail
+        output_string += '*el print, elset=elall\ns\n'
+        output_string += '*node file, output=2d, nset=nall\nu\n'
+        output_string += '*el file, elset=elall\ns,noe\n'
+        output_string += '*end step'
+
+        return output_string
+
+    def write_to_input_file(self,
+                            ccx_case_name: str) -> str:
+
+        if self.ccx_directory:
+            solver_path = os.path.join(os.getcwd(),
+                                       'ccx_files',
+                                       ccx_case_name)
+            os.mkdir(solver_path)
+
+        else:
+            solver_path = os.path.join(os.getcwd(),
+                                       ccx_case_name)
+
+        with open(os.path.join(solver_path, f'{ccx_case_name}.inp'),
+                  'w') as input_file:
+            input_file.writelines(self.translate_mesh())
+
+        return solver_path
+
+    def read_results(self,
+                     ccx_case_path: str,
+                     von_mises: bool = False):
+
+        if os.path.split(ccx_case_path)[-1].endswith('.frd'):
+            case_file = ccx_case_path
+
+        else:
+            case_file = os.path.join(
+                ccx_case_path,
+                f'{os.path.split(ccx_case_path)[-1]}.frd'
+>>>>>>> de028e5286366b5f95380719159af10ebdfd1db4
             )
 
         if von_mises:
@@ -135,6 +349,7 @@ def read_ccx_results(
             )
 
 
+<<<<<<< HEAD
 def run_ccx(
     mesh: Mesh,
     ccx_case_name: str,
@@ -146,6 +361,13 @@ def run_ccx(
 
     # Bura ccx
     ccx_name = "ccx_2.19"
+=======
+        # Bura ccx
+        # ccx_name = 'ccx_2.19'
+
+        # Home ccx
+        ccx_name = 'ccx'
+>>>>>>> de028e5286366b5f95380719159af10ebdfd1db4
 
     # Home ccx
     # ccx_name = "ccx"
--- pokreni_me_FWA.sh
+++ pokreni_me_FWA.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env sh
 
+<<<<<<< HEAD
 #SBATCH --job-name=FWA_kljesta
+=======
+#SBATCH --job-name=PSO_kljesta
+>>>>>>> de028e5286366b5f95380719159af10ebdfd1db4
 #SBATCH --partition=computes_thin
 #SBATCH -n 1
 #SBATCH --ntasks=48
 
+<<<<<<< HEAD
 out_name='kljesta_FWA'
 cd $out_name || exit
 
 python optimization.py
+=======
+out_name='kljesta_PSO'
+cd $out_name || exit
+
+<<<<<<<< HEAD:pokreni_me_PSO.sh
+python optimization.py
+========
+python3.9 optimization_PSO_from_ggs.py
+>>>>>>>> de028e5286366b5f95380719159af10ebdfd1db4:pokreni_me_FWA.sh
+>>>>>>> de028e5286366b5f95380719159af10ebdfd1db4
 
 cd ..
 
